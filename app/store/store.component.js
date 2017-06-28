2b03ae69d2a9fad07d4e73151c75b9b59aa96a37
Mike Hatfield <oakraven13@gmail.com>
2017-06-27 21:07:24 -0300

    Added CartSummary component

--- app/store/store.component.js
+++ app/store/store.component.js
@@ -8,56 +8,48 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
-var product_repository_1 = require('../model/product.repository');
-var StoreComponent = (function () {
-    function StoreComponent(repository) {
+const core_1 = require('@angular/core');
+const product_repository_1 = require('../model/product.repository');
+const cart_model_1 = require("../model/cart.model");
+let StoreComponent = class StoreComponent {
+    constructor(repository, cart) {
         this.repository = repository;
+        this.cart = cart;
         this.selectedCategory = null;
         this.productsPerPage = 4;
         this.selectedPage = 1;
     }
-    Object.defineProperty(StoreComponent.prototype, "products", {
-        get: function () {
-            var pageIndex = (this.selectedPage - 1) * this.productsPerPage;
-            return this.repository.getProducts(this.selectedCategory)
-                .slice(pageIndex, pageIndex + this.productsPerPage);
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(StoreComponent.prototype, "categories", {
-        get: function () {
-            return this.repository.getCategories();
-        },
-        enumerable: true,
-        configurable: true
-    });
-    StoreComponent.prototype.changeCategory = function (newCategory) {
+    get products() {
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
+    }
+    get categories() {
+        return this.repository.getCategories();
+    }
+    changeCategory(newCategory) {
         this.selectedCategory = newCategory;
-    };
-    StoreComponent.prototype.changePage = function (newPage) {
+    }
+    changePage(newPage) {
         this.selectedPage = newPage;
-    };
-    StoreComponent.prototype.changePageSize = function (newSize) {
+    }
+    changePageSize(newSize) {
         this.productsPerPage = Number(newSize);
         this.changePage(1);
-    };
-    Object.defineProperty(StoreComponent.prototype, "pageCount", {
-        get: function () {
-            return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
-        },
-        enumerable: true,
-        configurable: true
-    });
-    StoreComponent = __decorate([
-        core_1.Component({
-            selector: 'store',
-            moduleId: module.id,
-            templateUrl: 'store.component.html'
-        }), 
-        __metadata('design:paramtypes', [product_repository_1.ProductRepository])
-    ], StoreComponent);
-    return StoreComponent;
-}());
+    }
+    get pageCount() {
+        return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
+    }
+    addProductToCart(product) {
+        this.cart.addLine(product);
+    }
+};
+StoreComponent = __decorate([
+    core_1.Component({
+        selector: 'store',
+        moduleId: module.id,
+        templateUrl: 'store.component.html'
+    }), 
+    __metadata('design:paramtypes', [product_repository_1.ProductRepository, cart_model_1.Cart])
+], StoreComponent);
 exports.StoreComponent = StoreComponent;
